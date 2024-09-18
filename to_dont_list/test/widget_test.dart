c6355c2e8a445ad635109062ff233e0de0a21b4d
danielajgr <daniela92602@gmail.com>
2024-09-18 01:22:22 -0500

    tests

--- to_dont_list/test/widget_test.dart
+++ to_dont_list/test/widget_test.dart
@@ -29,7 +29,7 @@ void main() {
     expect(textFinder, findsOneWidget);
   });
 
-  testWidgets('ToDoListItem has a Circle Avatar with abbreviation',
+  testWidgets('ToDoListItem has a ClipRRect with amount and unit',
       (tester) async {
     await tester.pumpWidget(MaterialApp(
         home: Scaffold(
@@ -38,25 +38,36 @@ void main() {
                 completed: true,
                 onListChanged: (Ingredient item, bool completed) {},
                 onDeleteItem: (Ingredient item) {}))));
-    final abbvFinder = find.text('t');
-    final avatarFinder = find.byType(CircleAvatar);
+    final amountAndUnitFinder = find.text('1 Boxes(es)');
 
-    CircleAvatar circ = tester.firstWidget(avatarFinder);
+    ClipRRect circ = tester.firstWidget(amountAndUnitFinder);
     Text ctext = circ.child as Text;
 
     // Use the `findsOneWidget` matcher provided by flutter_test to verify
     // that the Text widgets appear exactly once in the widget tree.
-    expect(abbvFinder, findsOneWidget);
-    expect(circ.backgroundColor, Colors.black54);
-    expect(ctext.data, "t");
+    expect(amountAndUnitFinder, findsOneWidget);
+    expect(ctext.data, "1 Boxes(es)");
   });
 
-  testWidgets('Default ToDoList has one item', (tester) async {
+    testWidgets('Ingredients are grouped', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: ToDoList()));
 
-    final listItemFinder = find.byType(ToDoListItem);
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
 
-    expect(listItemFinder, findsOneWidget);
+    await tester.enterText(find.byType(TextField).first, 'Apples');
+    await tester.enterText(find.byType(TextField).at(1), '1');
+    await tester.tap(find.byType(DropdownButton<String>).at(0));
+    await tester.pump();
+    await tester.tap(find.text('Produce').last);
+    await tester.pump();
+
+    await tester.tap(find.byKey(const Key("OKButton")));
+    await tester.pump();
+
+    expect(find.text('Produce'), findsOneWidget);
+    expect(find.text('Apples'), findsOneWidget);
+    expect(find.text('1 CUP(s)'), findsOneWidget); 
   });
 
   testWidgets('Clicking and Typing adds item to ToDoList', (tester) async {
